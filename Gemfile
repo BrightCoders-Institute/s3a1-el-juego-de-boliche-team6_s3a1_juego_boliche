73e22f9aba53aa5ce1e5151dbc358ad34ab20552
Alejandro Escobar <AlejandroEscobarCampos>
2024-01-09 19:53:58 -0600

    feat: Start the bowling game
    Co-authored-by: CaroSer<adrianaserrano185@outlook.com>
    Co-authored-by:LuisChavez2023<ch.dl.luis20232024@gmail.com>
    Co-authored-by: Bryanroal<bryanroal27@gmail.com>

---
+++ Gemfile
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in s3a1-el-juego-de-boliche-team6_s3a1_juego_boliche.gemspec
+gemspec
+
+gem "rake", "~> 13.0"
---
+++ lib/s3a1/el/juego/de/boliche/team6_s3a1_juego_boliche.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require_relative "team6_s3a1_juego_boliche/version"
+
+module S3a1
+  module El
+    module Juego
+      module De
+        module Boliche
+          module Team6S3a1JuegoBoliche
+            class Error < StandardError; end
+            # Your code goes here...
+          end
+        end
+      end
+    end
+  end
+end
